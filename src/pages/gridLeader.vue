984d791d435264a8f17eb61f00f54fd0b96119a0
Soumita Nag <168756525+Soumita-Nag@users.noreply.github.com>
2025-07-21 10:46:18 +0530

    styleing done for leader side grid

--- src/pages/gridLeader.vue
+++ src/pages/gridLeader.vue
@@ -5,7 +5,7 @@
     </button>
 
     <div class="grid grid-cols-5 gap-2 w-full max-w-4xl">
-      <div v-for="(word, index) in props.wordList" :key="index" class=" cursor-pointer h-24 md:h-32 bg-white border rounded-lg shadow-sm flex justify-center items-center text-xl font-semibold hover:scale-105 hover:bg-blue-50 transition-all duration-200 ease-in-out" @dblclick="revealCell">
+      <div v-for="(word, index) in props.wordList" :key="index" class=" cursor-pointer h-24 md:h-32 bg-white border-4 text-gray-700 border-gray-500 rounded-lg shadow-sm flex justify-center items-center text-xl capitalize font-bold hover:scale-105 hover:bg-blue-50 transition-all duration-200 ease-in-out" :class="[word.colorCode==1&&word.revealed==0?'red':word.colorCode==2&&word.revealed==0?'blue':word.colorCode==3&&word.revealed==0?'black':'']" @dblclick="revealCell">
         {{ word.word }}
       </div>
     </div>
@@ -38,4 +38,22 @@
 </script>
 
 <style scoped>
+.red{
+  background-color: rgb(253, 126, 126);
+  border: 4px solid rgb(255, 77, 77);
+  border-radius: 10px;
+  color: rgb(89, 0, 0)
+}
+.blue{
+  background-color: rgb(121, 188, 255);
+  border: 4px solid rgb(61, 158, 255);
+  border-radius: 10px;
+  color: rgb(0, 22, 94)
+}
+.black{
+  background-color: rgb(116, 116, 116);
+  border: 4px solid rgb(60, 60, 60);
+  border-radius: 10px;
+  color: rgb(255, 255, 255)
+}
 </style>
