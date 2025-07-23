e601ef0bb366d038ab0ec18fd1b6ae1f8e5ed422
Soumita Nag <168756525+Soumita-Nag@users.noreply.github.com>
2025-07-24 01:44:03 +0530

    css modification

--- src/pages/gridTeam.vue
+++ src/pages/gridTeam.vue
@@ -5,7 +5,7 @@
     </div>
     <h1 v-if="props.wordList.length!=0" class="mb-10 text-white font-semibold text-sm sm:text-xl md:text-2xl">Unique Game Id: {{ props.gameId }}</h1>
     <div v-if="props.wordList.length!=0" class="grid grid-cols-5 gap-2 w-full max-w-4xl">
-      <div v-for="(word, index) in props.wordList" :key="index" class="cursor-pointer h-12 sm:h-20 md:h-26 bg-white border-4 text-gray-700 border-gray-500 rounded-lg shadow-sm flex justify-center items-center text-[0.65rem] sm:text-lg md:text-xl capitalize font-bold hover:scale-105 hover:bg-blue-50 transition-all duration-200 ease-in-out" :class="[word.colorCode==1&&word.revealed==1?'red':word.colorCode==2&&word.revealed==1?'blue':word.colorCode==3&&word.revealed==1?'black':word.colorCode==0&&word.revealed==1?'text-white hover:text-blue-50':'']" @dblclick="revealCell">
+      <div v-for="(word, index) in props.wordList" :key="index" class="cursor-pointer h-12 sm:h-20 md:h-26 bg-white border-2 sm:border-3 md:border-4 text-gray-700 border-gray-500 rounded-lg shadow-sm flex justify-center items-center text-[0.65rem] sm:text-lg md:text-xl capitalize font-bold hover:scale-105 hover:bg-blue-50 transition-all duration-200 ease-in-out" :class="[word.colorCode==1&&word.revealed==1?'red':word.colorCode==2&&word.revealed==1?'blue':word.colorCode==3&&word.revealed==1?'black':word.colorCode==0&&word.revealed==1?'text-white hover:text-blue-50':'']" @dblclick="revealCell">
         {{ word.word }}
       </div>
     </div>
